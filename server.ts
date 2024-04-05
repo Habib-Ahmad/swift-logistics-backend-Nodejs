7f113e14b184b348790c5225c87fe467d1cd9c93
Ahmad  Habib <TAJ531@tajbank.com>
2024-04-05 15:27:54 +0100

    Project setup complete with database connection

---
+++ server.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./config";
+
+dotenv.config();
+
+const app: Express = express();
+
+connectDb();
+
+const port = process.env.PORT || 8001;
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Swift Logistics Server!");
+});
+
+// app.use("/api/users", userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// app.use(errorHandler(false));
